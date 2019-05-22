9df178267494d166fac839b3b1f081a4e1253239
aabhas <mehtaaabhas@sheridancollege.ca>
2019-05-22 09:32:48 -0400

    first commit

---
+++ src/sw_week3/SW_week3.java
@@ -0,0 +1,24 @@
+/*
+ * Aabhas Mehta
+ * Student ID: 991 542 302
+ * SYST10199 - Web Programming
+ */
+package sw_week3;
+
+/**
+ *
+ * @author User
+ */
+public class SW_week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student s1 = new Student();
+        s1.setId(991542302);
+        s1.setFirstName("Aabhas");
+    }
+    
+}
---
+++ src/sw_week3/Student.java
@@ -0,0 +1,43 @@
+/*
+ * Aabhas Mehta
+ * Student ID: 991 542 302
+ * SYST10199 - Web Programming
+ */
+package sw_week3;
+
+/**
+ *
+ * @author User
+ */
+public class Student {
+    private int id;
+    private String firstName;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
